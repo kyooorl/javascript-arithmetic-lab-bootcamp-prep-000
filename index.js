dd4906159ff97e140afa5030e7980c6e324cc270
Monica Lee <mylee.715@gmail.com>
2019-05-30 20:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ function dec(a){
 function makeInt(a){
   return parseInt(a, 10);
 }
+
+function preserveDecimal(a){
+  return parseFloat(, 2.222);
+}
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
